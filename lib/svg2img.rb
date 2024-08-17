77f97035e7af4dab4042b4b33cf20acd66acc8b4
Orvar Segerström <orvarsegerstrom@gmail.com>
2024-08-17 15:03:41 +0200

    v0.1.1

--- lib/svg2img.rb
+++ lib/svg2img.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "svg2img/version"
-require_relative "svg2img/svg2img"
+
+begin
+  RUBY_VERSION =~ /(\d+\.\d+)/
+  require "svg2img/#{Regexp.last_match(1)}/svg2img"
+rescue LoadError
+  require 'svg2img/svg2img'
+end
+
 
 module Svg2Img
   class Error < StandardError; end
--- package.sh
+++ package.sh
@@ -6,3 +6,8 @@ PLATFORMS="x86_64-linux x86_64-linux-musl x86_64-darwin aarch64-linux aarch64-li
 for P in $PLATFORMS; do
   bundle exec rb-sys-dock -p $P --build -r 3.2,3.3
 done
+
+# cd pkg
+# for gem in pkg/*.gem; do
+#   gem push $gem
+# done
